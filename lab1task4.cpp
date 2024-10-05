c0370fe3b756358ceef22ee3f6867350be6910b2
Ihsan Saif <ihsansaifedwardion@gmail.com>
2024-10-05 13:46:43 +0500

    Write a program to create a dynamic array of user defined size. Size should be in the range of 0 to 15. Write a function FindLarge that should ask user to enter a non-negative number. Function should find the next largest number than the input number in the list.
    Sample input:
    Enter size: 5
    
    After insertion:
    13 4 55 29 32
    
    Sample output:
    Enter number: 15
    Next largest number from 15 is: 29

---
+++ lab1task4.cpp
@@ -0,0 +1,25 @@
+/*
+Write a program to create a dynamic array of user defined size. Size should be in the range
+of 0 to 15. Write a function FindLarge that should ask user to enter a non-negative number.
+Function should find the next largest number than the input number in the list.
+
+Sample input:
+Enter size: 5
+
+After insertion:
+13 4 55 29 32
+
+Sample output:
+Enter number: 15
+Next largest number from 15 is: 29
+
+*/
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+
+
+ return 0;
+}
