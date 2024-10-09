8f310548a0e95926d926e77ec184b7f4107bf3d7
Ihsan Saif <ihsansaifedwardion@gmail.com>
2024-10-09 12:27:48 +0500

    Write a program to implement a Stack using dynamic array of Character values. Implement the following functions in Stack with necessary checks: a) Insert value in Stack (PUSH) b) Delete value from Stack (POP) c) Display the Stack

---
+++ lab3task1.cpp
@@ -0,0 +1,66 @@
+/* Write a program to implement a Stack using dynamic array of Character values. Implement the
+following functions in Stack with necessary checks:
+a) Insert value in Stack (PUSH)
+b) Delete value from Stack (POP)
+c) Display the Stack
+*/
+#include<iostream>
+using namespace std;
+ class dynamic
+ {
+   char *stack1;
+   int size1;
+    int top;
+ public:
+ dynamic(int size)
+ {
+    size1=size;
+    stack1 = new char [size];
+    top=-1;
+ }
+ void push(int val)
+ {
+    if (top<size1)
+    {
+        top++;
+        stack1[top]=val;
+
+    }
+ }
+ void pop()
+ {
+    if (top>=0)
+    {
+    top--;
+    cout<<"Last item has been poped";
+    }
+    
+   
+ }
+ void display()
+ {
+    for (int i = 0; i <= top; i++)
+    {
+        cout<<stack1[i];
+    }
+    
+ }
+ };
+ 
+int main()
+{
+    int size=5;
+    dynamic d(size);
+    for (int i = 0; i < size; i++)
+    {
+        char val;
+        cout<<"Enter value at "<<i+1<<" : ";
+        cin>>val;
+        d.push(val);
+    }
+   //d.pop();
+    d.display();
+    
+
+ return 0;
+}
