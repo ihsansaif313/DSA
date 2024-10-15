47766ce1aee2ff5cecc1f5ec9235155d33e17e05
Ihsan Saif <ihsansaifedwardion@gmail.com>
2024-10-15 23:23:52 +0500

    updated

--- lab3task2.cpp
+++ lab3task2.cpp
@@ -29,7 +29,7 @@ class greaternum
 {
     int *arr;
     int size;
-    int top=0;
+    int top = 0;
 
 public:
     greaternum(int size1)
@@ -61,32 +61,68 @@ public:
             cout << "Enter the value to be pushed at position " << i + 1 << ": ";
             cin >> arr[i];
             top++;
-            
         }
-        if (top==size)
+        if (top == size)
         {
-            cout<<"Stack is full"<<endl;
+            cout << "Stack is full" << endl;
         }
-        
-        
     }
 
     void POP()
     {
-       if (top > -1)
-       {
-        top--;
-        cout<<"Element has been Poped out from Stack"<<endl;
-       }
-
-       
-
+        if (top > -1)
+        {
+            top--;
+            cout << "Element has been Poped out from Stack\n\n"
+                 << endl;
+        }
     }
     void display()
     {
+        for (int i = 0; i < top; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl
+             << endl;
     }
     void nextgreater()
     {
+        int next;
+        int topp;
+        cout<<"Numbers already in stack is: ";
+         for (int i = 0; i < top; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "Enter num to find the next Greater number on the stack : ";
+        cin >> next;
+
+        if (top != -1)
+        {
+            for (int i = 0; i <top; i++)
+            {
+                if(next==arr[i])
+                {
+                    topp=i;
+                    break;
+                }
+                else
+                {
+                    cout<<"Number you have entered is not available in stack. Check it again please "<<arr[i]<<endl;
+                    nextgreater();
+                }
+                
+                
+            }
+        }
+        else
+        {
+            cout<<"stack is empty!";
+        }
+        
     }
 };
 
@@ -96,32 +132,31 @@ int main()
     greaternum g(size);
     int choice = g.menu();
     while (choice != 0)
-    {  
-    switch (choice)
     {
-    case 0:
-        cout << "Program Terminated";
-        break;
-    case 1:
-        g.push();
-        choice=g.menu();
-        break;
-    case 2:
-        g.POP();
-        choice=g.menu();
-        break;
-    case 3:
-        g.display();
-        choice=g.menu();
-        break;
+        switch (choice)
+        {
+        case 0:
+            cout << "Program Terminated";
+            break;
+        case 1:
+            g.push();
+            choice = g.menu();
+            break;
+        case 2:
+            g.POP();
+            choice = g.menu();
+            break;
+        case 3:
+            g.display();
+            choice = g.menu();
+            break;
 
-    case 4:
-        g.nextgreater();
-        choice=g.menu();
-        break;
+        case 4:
+            g.nextgreater();
+            choice = g.menu();
+            break;
+        }
     }
-    }
-    
 
     return 0;
 }
